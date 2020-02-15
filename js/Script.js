e693a8e0a7ee8a57f312b8c73b39f4d85fad34f6
hinobi936 <hi-nobi-9@mail.ru>
2020-02-16 01:21:01 +0500

    js css

--- js/Script.js
+++ js/Script.js
@@ -1,34 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const cartBtn = document.getElementById('cart');
-    const cardWrapper = document.querySelector('.card-wrapper');
-    const modalInfo = document.getElementById('Modal_more');
-    const modalCart = document.getElementById('Modal_order');
-    let cardDeck = document.getElementById('card-deck');
-    let cartCounter = document.getElementById('cartCounter');
-    let wishList = [];
-    let cart = {};
+  const cartBtn = document.getElementById('cart');
+  const cardWrapper = document.querySelector('.card-wrapper');
+  const modalInfo = document.getElementById('Modal_more');
+  const modalCart = document.getElementById('Modal_order');
+  let cardDeck = document.getElementById('card-deck');
+  let cartCounter = document.getElementById('cartCounter');
+  const orderButton = document.getElementsByClassName('add-to-cart');
+  let wishList = [];
+  let cart = {};
 
-    // console.log(modalInfo);
+  console.log(orderButton);
 
-    // загрузка услуг на страницу
-    loadService();
-    checkCart();
-    checkCount();
+  // загрузка услуг на страницу
+  loadService();
+  checkCart();
+  checkCount();
 
-    const printServices = (out) => {
-        let card = document.createElement('div');
-        card.className = 'card-wrapper col-lg-4 col-md-6 col-sm-6 mb-4';
-        card.innerHTML = out;
-        cardDeck.append(card);
-    }
+  const printServices = (out) => {
+    let card = document.createElement('div');
+    card.className = 'card-wrapper col-lg-4 col-md-6 col-sm-6 mb-4';
+    card.innerHTML = out;
+    cardDeck.append(card);
+  }
 
-    function loadService() {
-        $.getJSON('service.json', function (data) {
-            // console.log(data);
-            let out;
-            for (var key in data) {
-                out = `<div class="card text-dark">
+  function loadService() {
+    $.getJSON('service.json', function (data) {
+      // console.log(data);
+      let out;
+      for (var key in data) {
+        out = `<div class="card text-dark">
                             <img src="${data[key]['img']}" alt=""
                                 class="card-img-top">
                             <div class="card-body text-center">
@@ -40,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             </div>
                             <div class="row">
                                 <div class="col-md-6">
-                                <button type="button" class="btn btn-primary more-info" data-id=${key} data-toggle="modal" data-target="#Modal_more">Подробнее
+                                <button type="button" class="btn btn-more more-info" data-id=${key} data-toggle="modal" data-target="#Modal_more">Подробнее
                                 </button>
                                 </div>
                                 <div class="col-md-6">
@@ -48,31 +49,31 @@ document.addEventListener('DOMContentLoaded', () => {
                                 </div>
                             </div>
                             </div>`
-                printServices(out);
-            }
-            // console.log(out);    
-        })
-    }
+        printServices(out);
+      }
+      // console.log(out);    
+    })
+  }
 
-    const addToCart = id => {
-        // добавление услуги в корзину
-        let serviceId = id;
-        if (cart[serviceId] != undefined) {
-            cart[serviceId]++;
-            // console.log(cart);
-        }
-        else {
-            cart[serviceId] = 1;
-            // console.log(cart);
-        }
-        localStorage.setItem('cart', JSON.stringify(cart));
+  const addToCart = id => {
+    // добавление услуги в корзину
+    let serviceId = id;
+    if (cart[serviceId] != undefined) {
+      cart[serviceId]++;
+      // console.log(cart);
     }
+    else {
+      cart[serviceId] = 1;
+      // console.log(cart);
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
 
-    const showMoreInfo = id => {
-        // modalInfo.innerHTML='';
-        let out;
-        $.getJSON('service.json', function (data) {
-            out = `<div class="modal-dialog modal-lg" role="document">
+  const showMoreInfo = id => {
+    // modalInfo.innerHTML='';
+    let out;
+    $.getJSON('service.json', function (data) {
+      out = `<div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
               <div class="modal-header">
                 <h4 class="modal-title">${data[id].type}</h4>
@@ -97,51 +98,71 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Оставить заявку</button>
+                <button type="button" class="btn btn-primary" data-id=${id}>Оставить заявку</button>
               </div>
             </div><!-- /.modal-content -->
           </div><!-- /.modal-dialog -->`;
-          console.log(out);
-          modalInfo.innerHTML=out;
-        })
-    }
+      console.log(out);
+      modalInfo.innerHTML = out;
+      // eval(modalInfo);
+    })
+  }
 
-    function checkCount() {
-        // подсчет количества в корзине
-        cartCounter.textContent = Object.keys(cart).length;
-        // console.log(cartCounter);
-    }
+  const changeButtonAdd = target => {
+    console.log(target.style);
+    target.textContent = 'В корзине';
+    target.style.backgroundColor = "#777777";
+  }
 
-    const handlerCard = event => {
-        // обработчик добавления в корзину и wishList
-        const target = event.target;
-        // добавить в корзину
-        if (target.classList.contains('add-to-cart')) {
-            // console.log(event);
-            // console.log(target.dataset);
-            addToCart(target.dataset.id);
-            checkCount();
+  function checkCount() {
+    // подсчет количества в корзине
+    cartCounter.textContent = Object.keys(cart).length;
+    // console.log(cartCounter);
+  }
+
+  const handlerCard = event => {
+    // обработчик добавления в корзину и wishList
+    const target = event.target;
+    // добавить в корзину
+    if (target.classList.contains('add-to-cart')) {
+      // console.log(event);
+      // console.log(target.dataset);
+      addToCart(target.dataset.id);
+      changeButtonAdd(target);
+      checkCount();
+    }
+    if (target.classList.contains('more-info')) {
+      // узнать подробнее
+      // console.log(target.dataset);
+      showMoreInfo(target.dataset.id);
+
+    }
+    // добавить в избранное
+  }
+
+  function checkCart() {
+    // проверка наличия корзины в localStorage
+    if (localStorage.getItem('cart') != null) {
+      cart = JSON.parse(localStorage.getItem('cart'));
+      for (let i = 0; i < orderButton.length; i++) {
+        for(let j=0; j<Object.keys(cart).length; j++){
+          id=orderButton[i].dataset.id;
+          console.log(id);
+        
+          if (cart.hasOwnProperty(id)){
+            changeButtonAdd(orderButton[i]);
+          }
         }
-        if (target.classList.contains('more-info')) {
-            // узнать подробнее
-            // console.log(target.dataset);
-            showMoreInfo(target.dataset.id);
+        console.log(orderButton[i]);
+      }
 
-        }
-        // добавить в избранное
+      console.log(Object.keys(cart).length);
     }
+    // console.log(localStorage.getItem('cart'));
+  }
 
-    function checkCart() {
-        // проверка наличия корзины в localStorage
-        if (localStorage.getItem('cart') != null) {
-            cart = JSON.parse(localStorage.getItem('cart'));
-            // console.log(cart);
-        }
-        // console.log(localStorage.getItem('cart'));
-    }
-
-    // прослушиватель событий кнопок в "услугах"
-    cardDeck.addEventListener('click', handlerCard);
+  // прослушиватель событий кнопок в "услугах"
+  cardDeck.addEventListener('click', handlerCard);
 
 
 
